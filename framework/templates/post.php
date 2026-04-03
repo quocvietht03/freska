1b36bf3ea477968f9c562bfe8741e5af9100f976
ADMIN <giabaotruong300704@gmail.com>
2026-04-03 15:41:52 +0700

    add widget style 2, 3, single post

--- framework/templates/post.php
+++ framework/templates/post.php
@@ -5,9 +5,8 @@ $layout = !empty($args['layout']) ? $args['layout'] : 'layout-default';
     <?php if ($layout != 'layout-02') : ?>
         <div class="bt-post--infor">
             <?php
-            echo freska_post_category_render();
             echo is_single() ? freska_single_post_title_render() : freska_post_title_render();
-            echo freska_post_meta_single_render();
+            echo freska_post_meta_category_render();
             ?>
         </div>
     <?php endif; ?>
